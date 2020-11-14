433726c4233ee766b262af3f3191212794a71881
Feffa-hub <66078455+Feffa-hub@users.noreply.github.com>
2020-11-14 12:55:03 +0100

    Added source link for GitHub in credits

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function footer() {
   return (
     <footer className="footer-code">
-      <small>Open-source code , by Federica Costa</small>
+    <small><a href ="https://github.com/Feffa-hub/react-weather" target="_blanck">Open-source code</a>, by Federica Costa</small>
     </footer>
   );
 }
