23749e776a6761c6b59e3e07ba5e35cfcbba0858
Feffa-hub <66078455+Feffa-hub@users.noreply.github.com>
2020-11-14 12:58:05 +0100

    Fixed error in credits

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function footer() {
   return (
     <footer className="footer-code">
-    <small><a href ="https://github.com/Feffa-hub/react-weather" target="_blanck">Open-source code</a>, by Federica Costa</small>
+    <small><a href ="https://github.com/Feffa-hub/react-weather" target="_blank" rel="noreferrer">Open-source code</a>, by Federica Costa</small>
     </footer>
   );
 }
