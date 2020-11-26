f4998c62befed6d1484877ae02de16400d6bdec3
Feffa-hub <66078455+Feffa-hub@users.noreply.github.com>
2020-11-26 17:44:57 +0100

    Create Footer.css component and Update it

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Footer.css"; 
 
 
 export default function footer() {
   return (
     <footer className="footer-code">
-    <small><a href ="https://github.com/Feffa-hub/react-weather" target="_blank" rel="noreferrer">Open-source code</a>, by Federica Costa</small>
+    <a href ="https://github.com/Feffa-hub/react-weather" target="_blank" rel="noreferrer">Open-source code</a>, by Federica Costa
     </footer>
   );
 }
